d4ae993b33a9da31d7ee79424bac3713cca346a4
Liron Levy <lironpro14@gmail.com>
2019-01-07 00:36:06 +0200

    added third person character with animations

---
+++ Assets/Scripts/SciFiSoldierController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SciFiSoldierController : MonoBehaviour
+{
+    Actions actions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        actions = GetComponent<Actions>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveForwardAmount = Input.GetAxis("Vertical");
+        float rotateAmount = Input.GetAxis("Horizontal");
+        float mouseMoveAmount = Input.GetAxis("Mouse X");
+        
+        if(Mathf.Abs(moveForwardAmount) > 0) {
+            actions.Walk();
+        } else {
+            actions.Stay();
+        }
+        GetComponent<Transform>().position += Camera.main.transform.forward*moveForwardAmount*Time.fixedDeltaTime;
+        GetComponent<Transform>().RotateAround(GetComponent<Transform>().position,Vector3.up,rotateAmount*Time.fixedDeltaTime*40);
+
+    }
+}
